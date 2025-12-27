0f0224e69878c89ce5b168e27be05f05f8b66cde
Bruce Reeves <js.python03@gmail.com>
2025-12-27 13:00:32 -0800

    programmed dialog.tsx

---
+++ src/app/components/ui/command.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import * as React from "react";
+import { Command as CommandPrimitive } from "cmdk";
+import { SearchIcon } from "lucide-react";
+
+import { cn } from "./utils.ts";
+import {
+Dialog,
+DialogContent,
+DialogDescription,
+DialogHeader,
+DialogTitle,
+} from "./dialog";
+
+// Not finished 27/12/2025
