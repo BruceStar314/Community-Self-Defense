1ecf3c8944580f4ba3b1d095f7d92f3fd8524fe3
Bruce Reeves <js.python03@gmail.com>
2026-01-03 06:16:51 -0800

    added under construction

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function SchedulePage() {
   return (
-    <div className="p-12">
-      <h1 className="text-4xl mb-6">Class Schedule</h1>
-      <p>Schedule content goes here.</p>
+    <div className="h-screen w-full bg-gray-300 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl mb-4 font-bold">Under Construction</h1>
+        <p className="text-lg">Coming soon.</p>
+        <Link href="/" className="text-l h-3xl w-xl bg-gray-400 rounded-xl p-1">Go back to home page</Link>
+      </div>
     </div>
   );
 }
